77f650efacbc4ce0dbfac5d2f35bb4ebbdc66177
Carlos Perales <carlosperales050@gmail.com>
2023-03-22 18:54:47 -0600

    se agregan nodes

---
+++ src/nodes.js
@@ -0,0 +1,28 @@
+// sections
+const trendingPreviewSection=document.querySelector('#trendingPreview');
+const categoriesPreviewSection=document.querySelector('.categoriesPreview');
+const genericListSection=document.querySelector('#genericList');
+const movieDetailSection =document.querySelector('#movieDetail');
+const relatedMoviesContainer=document.querySelector('.relatedMovies-container');
+
+
+//list & containers
+const trendingMoviesPreviewList=document.querySelector('.trendingPreview-movieList');
+const trendingMoviesPreviewListTitle=document.querySelector('trendingPreview-title');
+
+const relatedMoviesScrollContainer=document.querySelector('.relatedMovies-scrollContainer');
+
+//elements
+const headerArrow= document.querySelector('.header-arrow');
+const headerTitle=document.querySelector('.header-title');
+const headerTitleCategoryViewer=document.querySelector('.header-title--categoryViewer');
+
+const searchForm =document.querySelector('#searchForm');
+const search=document.querySelector('#search');
+const searchBtn=document.querySelector('#searchBtn');
+
+const movieDetailTitle=document.querySelector('.movieDetail-title');
+const movieDetailScore=document.querySelector('.movieDetail-score');
+const movieDetailDescription=document.querySelector('.movieDetail-description');
+
+const relatedMoviesTitle=document.querySelector('.relatedMovies-title')
